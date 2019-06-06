f9d31af227850cafdf4b9e6d0ef7ce88ef9ae4b3
TiffanyWangEngineer <tiffany.wang.engineer@gmail.com>
2019-06-06 14:18:45 -0700

    Green circle & Hello world main.js code.

--- main.js
+++ main.js
@@ -3,12 +3,25 @@ $(document).ready(function() {
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
     
+    /* Draw green dots when click mouse.*/
+
+    /*
     var tool = new Tool();
     tool.onMouseDown = function(event) {
         var c = paper.Shape.Circle([event.point.x, event.point.y], 20);
         c.fillColor = 'green';
     };
+    paper.view.draw();
+    */
+   
+    /* Green Circle and Hello world. */
+    var c = Shape.Circle(200, 200,80);
+    c.fillColor = 'green';
+    var text = new PointText(200, 200);
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontsize = 20;
+    text.content = 'Hello world.';
 
-    paper.view.draw();           
     console.log("main.js loaded");
 });
